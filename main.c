bca8f052e74b508e73fbbdb46b0b45fccde0d37c
Sebastian LaVine <mail@smlavine.com>
2021-06-27 13:30:23 -0400

    Switch to a multi-file license header style

--- main.c
+++ main.c
@@ -1,20 +1,21 @@
-/*
+/**
  * snake - A small TUI snake game
  * Copyright (C) 2021 Sebastian LaVine <mail@smlavine.com>
  *
- * This program is free software: you can redistribute it and/or modify
+ * This file is part of snake.
+ * snake is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * snake is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>. 
- */
+ * along with snake.  If not, see <https://www.gnu.org/licenses/>.
+ **/
 
 #include <assert.h>
 #include <signal.h>
